7f37ad070b221642a3403aa04db1932098a32e90
Johan-web-developer <johan.flore.13@gmail.com>
2023-06-13 13:03:22 -0500

    build: :rocket:

---
+++ psicology/vista/modules/sidebar.php
@@ -0,0 +1,106 @@
+<aside class="main-sidebar sidebar-dark-gray elevation-4">
+    <!-- Brand Logo -->
+    <a href="../../index3.html" class="brand-link">
+      <img src="vista/assets/images/AdminLTELogo.png" alt="AdminLTE Logo" class="brand-image img-circle elevation-3" style="opacity: .8">
+      <span class="brand-text font-weight-light">Psichology</span>
+    </a>
+
+    <!-- Sidebar -->
+    <div class="sidebar">
+      <!-- Sidebar user (optional) -->
+      <div class="user-panel mt-3 pb-3 mb-3 d-flex">
+        <div class="image">
+          <img src="vista/assets/images/angie.jpeg" class="img-circle elevation-2" alt="User Image">
+        </div>
+        <div class="info">
+          <a href="#" class="d-block">Angie Suarez</a>
+        </div>
+      </div>
+
+      <!-- SidebarSearch Form -->
+      <div class="form-inline">
+        <div class="input-group" data-widget="sidebar-search">
+          <input class="form-control form-control-sidebar" type="search" placeholder="Search" aria-label="Search">
+          <div class="input-group-append">
+            <button class="btn btn-sidebar">
+              <i class="fas fa-search fa-fw"></i>
+            </button>
+          </div>
+        </div>
+      </div>
+
+      <!-- Sidebar Menu -->
+      <nav class="mt-2">
+        <ul class="nav nav-pills nav-sidebar flex-column" data-widget="treeview" role="menu" data-accordion="false">
+          <!-- Add icons to the links using the .nav-icon class
+               with font-awesome or any other icon font library -->
+        <li class="nav-item">
+            <a href="/users" class="nav-link">
+            <i class="nav-icon far fa-user"></i>
+              <p>
+                User
+              </p>
+            </a>
+        </li>
+        <li class="nav-item">
+            <a href="/users" class="nav-link">
+            <i class="nav-icon far fa-user"></i>
+              <p>
+                Clinic History
+              </p>
+            </a>
+        </li>
+        <li class="nav-item">
+            <a href="/users" class="nav-link">
+            <i class="nav-icon far fa-user"></i>
+              <p>
+                Interviews
+              </p>
+            </a>
+        </li>
+        <li class="nav-item">
+            <a href="/users" class="nav-link">
+            <i class="nav-icon far fa-user"></i>
+              <p>
+                Psichlogits
+              </p>
+            </a>
+        </li>
+        <li class="nav-item">
+            <a href="/users" class="nav-link">
+            <i class="nav-icon far fa-user"></i>
+              <p>
+                Treatment
+              </p>
+            </a>
+        </li>
+        <li class="nav-item">
+            <a href="#" class="nav-link">
+              <i class="nav-icon far fa-envelope"></i>
+              <p>
+                Mailbox
+                <i class="fas fa-angle-left right"></i>
+              </p>
+            </a>
+            <ul class="nav nav-treeview">
+              <li class="nav-item">
+                <a href="../mailbox/mailbox.html" class="nav-link">
+                  <i class="far fa-circle nav-icon"></i>
+                  <p>Inbox</p>
+                </a>
+              </li>
+              <li class="nav-item">
+                <a href="../mailbox/compose.html" class="nav-link">
+                  <i class="far fa-circle nav-icon"></i>
+                  <p>Compose</p>
+                </a>
+              </li>
+              <li class="nav-item">
+                <a href="../mailbox/read-mail.html" class="nav-link">
+                  <i class="far fa-circle nav-icon"></i>
+                  <p>Read</p>
+                </a>
+              </li>
+            </ul>
+          </li>
+        
